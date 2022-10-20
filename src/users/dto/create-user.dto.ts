d6449efcf11a72f4085129eba7057821ec941807
pshabanov <pshabanov@nurtelecom.kg>
2022-10-20 12:13:18 +0600

    user docs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+
+    @ApiProperty({example: 'user@email.com', description: 'Почта'})
     readonly email: string
+
+    @ApiProperty({example: 'hf21F_qg5[`gh~0~`]bd', description: 'Почта'})
     readonly password: string
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface UserCreationAttrs {
     email: string
@@ -8,18 +9,23 @@ interface UserCreationAttrs {
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs>{
 
+    @ApiProperty({example: 1, description: 'Уникальный идентификатор'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number
 
+    @ApiProperty({example: 'user@email.com', description: 'Уникальный email, не может быть пустым'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string
 
+    @ApiProperty({example: 'Gdf;#$516g[Gls~gd', description: 'Пароль, не может быть пустым'})
     @Column({type: DataType.STRING, allowNull: false})
     password: string
 
+    @ApiProperty({example: true, description: 'Статус бана, по умолчанию - нет'})
     @Column({type: DataType.BOOLEAN, defaultValue: false})
     banned: boolean
 
-    @Column({type: DataType.STRING, unique: true, allowNull: true})
+    @ApiProperty({example: 'Оскорбление пользователей', description: 'Причина бана'})
+    @Column({type: DataType.STRING, allowNull: true})
     banReason: string
 }
